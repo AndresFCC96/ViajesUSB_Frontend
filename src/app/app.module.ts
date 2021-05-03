59bcaa66ab7108ad4b7ad1736d37a67ef7dda807
AndresFCC <dtmo96@outlook.com>
2021-05-03 04:29:03 -0500

    Contenido grafico añadido

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './component/navbar/navbar.component';
 import { FooterComponent } from './component/footer/footer.component';
 import { HeaderComponent } from './component/header/header.component';
-import { ComponentComponent } from './component/component.component';
 import { CoverComponent } from './component/cover/cover.component';
+import { ProductosComponent } from './component/productos/productos.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
     FooterComponent,
     HeaderComponent,
-    ComponentComponent,
-    CoverComponent
+    CoverComponent,
+    ProductosComponent
   ],
   imports: [
     BrowserModule
