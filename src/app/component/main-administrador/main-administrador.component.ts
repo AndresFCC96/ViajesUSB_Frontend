641bb606a8eee87da3df56b299b63cb1958d22f3
AndresFCC <dtmo96@outlook.com>
2021-05-04 21:27:23 -0500

    Pagina principal terminada faltan los modulos de administrador

---
+++ src/app/component/main-administrador/main-administrador.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-main-administrador',
+  templateUrl: './main-administrador.component.html',
+  styleUrls: ['./main-administrador.component.css']
+})
+export class MainAdministradorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
